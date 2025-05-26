a53ee9f3f8b16d086a1c16518f7f8f81158a3c70
lorenzo-pa <lorenzo.giorgi@pagopa.it>
2025-05-26 16:31:11 +0200

    Removed order by clause that is not supported in redshift materialized views

--- views/application/R__application_01_service_elapsed_mv.sql
+++ views/application/R__application_01_service_elapsed_mv.sql
@@ -7,5 +7,4 @@ DROP MATERIALIZED VIEW IF EXISTS views.mv_service_endpoint_method_ms_elapsed;
 CREATE MATERIALIZED VIEW views.mv_service_endpoint_method_ms_elapsed AS 
 select EXTRACT(year from bra.timestamp_tz) as year,EXTRACT(month from bra.timestamp_tz) as month, EXTRACT(day from bra.timestamp_tz) as day,  bra.service, bra.endpoint, bra.http_method, AVG(era."timestamp" - bra."timestamp") as ms_avg_elapsed  from interop_dev.application.begin_request_audit bra left join
 interop_dev.application.end_request_audit era on bra.span_id = era.span_id
-group by EXTRACT(year from bra.timestamp_tz), EXTRACT(month from bra.timestamp_tz), EXTRACT(day from bra.timestamp_tz), bra.service, bra.endpoint, bra.http_method
-order by AVG(era."timestamp" - bra."timestamp") desc;
+group by EXTRACT(year from bra.timestamp_tz), EXTRACT(month from bra.timestamp_tz), EXTRACT(day from bra.timestamp_tz), bra.service, bra.endpoint, bra.http_method;
