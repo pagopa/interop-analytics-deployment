3d2a88278bd5d0a002a8fa77757f9824e45ef0d9
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2025-05-27 12:00:49 +0200

    fix(mv): edit mv creation by adding AUTO REFRESH clause and using DATE_PART function (#68)

--- views/application/R__application_01_service_elapsed_mv.sql
+++ views/application/R__application_01_service_elapsed_mv.sql
@@ -4,7 +4,15 @@ GRANT USAGE ON SCHEMA views TO GROUP readonly_group;
 
 DROP MATERIALIZED VIEW IF EXISTS views.mv_service_endpoint_method_ms_elapsed;
 
-CREATE MATERIALIZED VIEW views.mv_service_endpoint_method_ms_elapsed  AS 
-select EXTRACT(year from bra.timestamp_tz) as year,EXTRACT(month from bra.timestamp_tz) as month, EXTRACT(day from bra.timestamp_tz) as day,  bra.service, bra.endpoint, bra.http_method, AVG(era."timestamp" - bra."timestamp") as ms_avg_elapsed  from interop_dev.application.begin_request_audit bra inner join
-interop_dev.application.end_request_audit era on bra.span_id = era.span_id
-group by EXTRACT(year from bra.timestamp_tz), EXTRACT(month from bra.timestamp_tz), EXTRACT(day from bra.timestamp_tz), bra.service, bra.endpoint, bra.http_method;
+CREATE MATERIALIZED VIEW views.mv_service_endpoint_method_ms_elapsed AUTO REFRESH YES AS 
+select 
+DATE_PART(year, (CONVERT_TIMEZONE('Europe/Rome',  TIMESTAMP 'epoch' + (bra.timestamp / 1000) * INTERVAL '1 second'))) as year, 
+DATE_PART(month, (CONVERT_TIMEZONE('Europe/Rome',  TIMESTAMP 'epoch' + (bra.timestamp / 1000) * INTERVAL '1 second'))) as month, 
+DATE_PART(day, (CONVERT_TIMEZONE('Europe/Rome',  TIMESTAMP 'epoch' + (bra.timestamp / 1000) * INTERVAL '1 second'))) as day, 
+bra.service, bra.endpoint, bra.http_method, AVG(era."timestamp" - bra."timestamp") as ms_avg_elapsed 
+from interop_dev.application.begin_request_audit bra inner join interop_dev.application.end_request_audit era on bra.span_id = era.span_id
+group by 
+DATE_PART(year, (CONVERT_TIMEZONE('Europe/Rome',  TIMESTAMP 'epoch' + (bra.timestamp / 1000) * INTERVAL '1 second'))), 
+DATE_PART(month, (CONVERT_TIMEZONE('Europe/Rome',  TIMESTAMP 'epoch' + (bra.timestamp / 1000) * INTERVAL '1 second'))), 
+DATE_PART(day, (CONVERT_TIMEZONE('Europe/Rome',  TIMESTAMP 'epoch' + (bra.timestamp / 1000) * INTERVAL '1 second'))), 
+bra.service, bra.endpoint, bra.http_method;
