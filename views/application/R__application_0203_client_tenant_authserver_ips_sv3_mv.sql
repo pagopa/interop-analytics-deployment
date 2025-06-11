7cd643f6cbc6e97e474d4c300c8faa1a8b646845
lorenzo-pa <lorenzo.giorgi@pagopa.it>
2025-06-11 14:41:38 +0200

    Refactored the ip view with the sub-views approac

---
+++ views/application/R__application_0203_client_tenant_authserver_ips_sv3_mv.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA IF NOT EXISTS sub_views;
+
+GRANT USAGE ON SCHEMA sub_views TO GROUP readonly_group;
+
+CREATE MATERIALIZED VIEW sub_views.mv_client_tenant_authserver_ips__oldest_ts AUTO REFRESH YES as
+select gta.client_id, bra.requester_ip_address ips, min(bra.timestamp) as oldest_ts from application.begin_request_audit bra
+inner join jwt.generated_token_audit gta on bra.correlation_id = gta.correlation_id
+where bra.service = 'authorization-server'
+group by gta.client_id, bra.requester_ip_address
