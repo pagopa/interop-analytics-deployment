3710afa87a5c42c919907092081e46336ed48025
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-06-16 14:59:33 +0200

    fix mv prefix name

--- views/application/R__application_00_service_elapsed_mv.sql
+++ views/application/R__application_00_service_elapsed_mv.sql
@@ -2,9 +2,9 @@ CREATE SCHEMA IF NOT EXISTS views;
 
 GRANT USAGE ON SCHEMA views TO GROUP readonly_group;
 
-DROP MATERIALIZED VIEW IF EXISTS views.00_mv_service_endpoint_method_ms_elapsed;
+DROP MATERIALIZED VIEW IF EXISTS views.mv_00_service_endpoint_method_ms_elapsed;
 
-CREATE MATERIALIZED VIEW views.00_mv_service_endpoint_method_ms_elapsed AUTO REFRESH YES AS 
+CREATE MATERIALIZED VIEW views.mv_00_service_endpoint_method_ms_elapsed AUTO REFRESH YES AS 
 select 
 DATE_PART(year, (CONVERT_TIMEZONE('Europe/Rome',  TIMESTAMP 'epoch' + (bra.timestamp / 1000) * INTERVAL '1 second'))) as year, 
 DATE_PART(month, (CONVERT_TIMEZONE('Europe/Rome',  TIMESTAMP 'epoch' + (bra.timestamp / 1000) * INTERVAL '1 second'))) as month, 
--- views/application/R__application_01_client_tenant_authserver_ips_mv.sql
+++ views/application/R__application_01_client_tenant_authserver_ips_mv.sql
@@ -2,9 +2,9 @@ CREATE SCHEMA IF NOT EXISTS views;
 
 GRANT USAGE ON SCHEMA views TO GROUP readonly_group;
 
-DROP MATERIALIZED VIEW IF EXISTS views.01_mv_client_tenant_authserver_ips;
+DROP MATERIALIZED VIEW IF EXISTS views.mv_01_client_tenant_authserver_ips;
 
-CREATE MATERIALIZED VIEW views.01_mv_client_tenant_authserver_ips AS 
+CREATE MATERIALIZED VIEW views.mv_01_client_tenant_authserver_ips AS 
 select  main.tenant_name, main.client_name, main.ips, latest_ts.latest_ts, oldest_ts.oldest_ts from sub_views.00_mv_client_tenant_authserver_ips__main main
 inner join sub_views.00_mv_client_tenant_authserver_ips__latest_ts latest_ts
 on main.client_id = latest_ts.client_id and main.ips = latest_ts.ips
