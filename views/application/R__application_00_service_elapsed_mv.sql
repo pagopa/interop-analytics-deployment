a796ba9b5f154bfaaee0ec6ea6f0d3985a9034fd
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-06-18 10:19:59 +0200

    add GRANT USAGE and GRANT SELECT ON materialized views to quicksight user

--- views/application/R__application_00_service_elapsed_mv.sql
+++ views/application/R__application_00_service_elapsed_mv.sql
@@ -1,6 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS views;
 
 GRANT USAGE ON SCHEMA views TO GROUP readonly_group;
+GRANT USAGE ON SCHEMA views TO interop_analytics_quicksight_user;
 
 DROP MATERIALIZED VIEW IF EXISTS views.mv_00_service_endpoint_method_ms_elapsed;
 
@@ -11,3 +12,5 @@ DATE_PART(month, (CONVERT_TIMEZONE('Europe/Rome',  TIMESTAMP 'epoch' + (bra.time
 DATE_PART(day, (CONVERT_TIMEZONE('Europe/Rome',  TIMESTAMP 'epoch' + (bra.timestamp / 1000) * INTERVAL '1 second'))) as day, 
 bra.service, bra.endpoint, bra.http_method, era."timestamp" - bra."timestamp" as ms_avg_elapsed 
 from interop_dev.application.begin_request_audit bra inner join interop_dev.application.end_request_audit era on bra.span_id = era.span_id
+
+GRANT SELECT ON TABLE views.mv_00_service_endpoint_method_ms_elapsed TO interop_analytics_quicksight_user;
