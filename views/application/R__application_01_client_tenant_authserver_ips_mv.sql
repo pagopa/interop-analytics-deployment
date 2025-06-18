f25ac62312a9083e8e72a04c32119213cc2652ca
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2025-06-18 10:28:33 +0200

    feat(mv): add GRANT USAGE and GRANT SELECT ON views to quicksight user (#87)

--- views/application/R__application_01_client_tenant_authserver_ips_mv.sql
+++ views/application/R__application_01_client_tenant_authserver_ips_mv.sql
@@ -1,6 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS views;
 
 GRANT USAGE ON SCHEMA views TO GROUP readonly_group;
+GRANT USAGE ON SCHEMA views TO interop_analytics_quicksight_user;
 
 DROP MATERIALIZED VIEW IF EXISTS views.mv_01_client_tenant_authserver_ips;
 
@@ -10,3 +11,5 @@ inner join sub_views.mv_00_client_tenant_authserver_ips__latest_ts latest_ts
 on main.client_id = latest_ts.client_id and main.ips = latest_ts.ips
 inner join sub_views.mv_00_client_tenant_authserver_ips__oldest_ts oldest_ts
 on main.client_id = oldest_ts.client_id and main.ips = oldest_ts.ips
+
+GRANT SELECT ON TABLE views.mv_01_client_tenant_authserver_ips TO interop_analytics_quicksight_user;
