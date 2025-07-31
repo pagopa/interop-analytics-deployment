402dd014e58fc3fe7ce3e504e6248010896f1d2d
Marco Vit <marco.vit@pagopa.it>
2025-07-31 09:42:48 +0200

    fix(views): select time interval of 5/15/30 minutes ago, not 5/10/15

--- views/application/auth_server_usage/R__application_mv_01_auth_usage__data__last_calls.sql
+++ views/application/auth_server_usage/R__application_mv_01_auth_usage__data__last_calls.sql
@@ -7,8 +7,8 @@ DROP MATERIALIZED VIEW IF EXISTS views.mv_01_auth_usage__data__last_ko_calls CAS
 
 -- Repeat the same query with three time different time period 
 --  Last 5 entire minutes. At 14:05:06 ithis interval is [ 14:00:00, 14:05:00 [
---  Last 10 entire minutes. At 14:05:06 ithis interval is [ 13:55:00, 14:05:00 [
 --  Last 15 entire minutes. At 14:05:06 ithis interval is [ 13:50:00, 14:05:00 [
+--  Last 30 entire minutes. At 14:05:06 ithis interval is [ 13:35:00, 14:05:00 [
 CREATE MATERIALIZED VIEW views.mv_01_auth_usage__data__last_calls AUTO REFRESH YES as
   select 
     consumer_name,
@@ -26,29 +26,7 @@ CREATE MATERIALIZED VIEW views.mv_01_auth_usage__data__last_calls AUTO REFRESH Y
   from 
     views.mv_00_auth_usage__data__calls
   where 
-    minute_slot between date_add('minute', -1 * 5, date_trunc( 'minute', getdate() ) )  
-                    and date_trunc( 'minute', getdate() )
-  group by 
-    consumer_name,
-    client_name
-UNION ALL 
-  select 
-    consumer_name,
-    client_name,
-    sum( calls_quantity ) as calls_quantity,
-    sum( total_execution_time ) as total_execution_time,
-    sum( quantity_2xx ) as quantity_2xx,
-    sum( total_2xx_execution_time ) as total_2xx_execution_time,
-    sum( quantity_4xx ) as quantity_4xx,
-    sum( total_4xx_execution_time ) as total_4xx_execution_time,
-    sum( quantity_5xx ) as quantity_5xx,
-    date_add('minute', -1 * 10, date_trunc( 'minute', getdate() ) ) as from_ts,
-    date_trunc( 'minute', getdate() ) as to_ts,
-    10 as period_length_minutes
-  from 
-    views.mv_00_auth_usage__data__calls
-  where 
-    minute_slot between date_add('minute', -1 * 10, date_trunc( 'minute', getdate() ) )  
+    minute_slot between date_add('minute', -1 * 5, date_trunc( 'minute', getdate() ) )
                     and date_trunc( 'minute', getdate() )
   group by 
     consumer_name,
@@ -70,7 +48,29 @@ UNION ALL
   from 
     views.mv_00_auth_usage__data__calls
   where 
-    minute_slot between date_add('minute', -1 * 15, date_trunc( 'minute', getdate() ) )  
+    minute_slot between date_add('minute', -1 * 15, date_trunc( 'minute', getdate() ) )
+                    and date_trunc( 'minute', getdate() )
+  group by 
+    consumer_name,
+    client_name
+UNION ALL 
+  select 
+    consumer_name,
+    client_name,
+    sum( calls_quantity ) as calls_quantity,
+    sum( total_execution_time ) as total_execution_time,
+    sum( quantity_2xx ) as quantity_2xx,
+    sum( total_2xx_execution_time ) as total_2xx_execution_time,
+    sum( quantity_4xx ) as quantity_4xx,
+    sum( total_4xx_execution_time ) as total_4xx_execution_time,
+    sum( quantity_5xx ) as quantity_5xx,
+    date_add('minute', -1 * 30, date_trunc( 'minute', getdate() ) ) as from_ts,
+    date_trunc( 'minute', getdate() ) as to_ts,
+    30 as period_length_minutes
+  from 
+    views.mv_00_auth_usage__data__calls
+  where 
+    minute_slot between date_add('minute', -1 * 30, date_trunc( 'minute', getdate() ) )
                     and date_trunc( 'minute', getdate() )
   group by 
     consumer_name,
