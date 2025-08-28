1618cbb3422d4748eebe0fd489879fc683fb74df
Marco Vit <marco.vit@pagopa.it>
2025-08-28 13:01:58 +0200

    Remove AUTO REFRESH where not supported

--- views/application/auth_server_usage/R__application_mv_01_auth_usage__checks__union.sql
+++ views/application/auth_server_usage/R__application_mv_01_auth_usage__checks__union.sql
@@ -11,7 +11,7 @@ DROP MATERIALIZED VIEW IF EXISTS sub_views.mv_01_auth_usage__checks__union CASCA
 --    `epoch_of_the_second_when_the_minute_slot_is_started` is not guaranteed to be the 
 --    same in both sub_views.
 --  - Incremental refresh do not support `union all` and `group by` in the same view
-CREATE MATERIALIZED VIEW sub_views.mv_01_auth_usage__checks__union AUTO REFRESH YES as
+CREATE MATERIALIZED VIEW sub_views.mv_01_auth_usage__checks__union as
 select 
   token_issued,
   token_issued_without_correlation_id,
