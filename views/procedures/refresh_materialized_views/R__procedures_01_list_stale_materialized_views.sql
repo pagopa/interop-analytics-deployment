44a4b7a462e30cde76aa4b238164a7c537b19898
marco-vit-pagopa <81150347+marco-vit-pagopa@users.noreply.github.com>
2025-09-12 16:55:41 +0200

    feature(mv-refresh): Add info to stale materialized views listing (#131) (PIN-7095)
    Add some information to table returned by sub_views.list_stale_materialized_views
    - incremental_refresh_not_supported: true if and only if state field of the SVV_MV_INFO table is 1.
    - last_refresh_start_time: timestamp, in iso format, when the materialized view last refresh started.
    - last_refresh_end_time: timestamp when the materialized view last refresh ended.
    - last_refresh_start_time_epoch & last_refresh_end_time_epoch: same timestamps as above but in epoch seconds, UTC, time; easier to parse.

--- views/procedures/refresh_materialized_views/R__procedures_01_list_stale_materialized_views.sql
+++ views/procedures/refresh_materialized_views/R__procedures_01_list_stale_materialized_views.sql
@@ -23,33 +23,61 @@ BEGIN
   create temporary table list_need_refresh_views_results AS 
   (
     with
-        views_to_refresh as (
-          select
-            trim(schema_name) as mv_schema,
-            trim(name) as mv_name,
-            cast(REGEXP_REPLACE( name, 'mv_([0-9][0-9]).*', '$1') as integer) as mv_level 
-          from 
-            SVV_MV_INFO
-          where
-              is_stale = 't'
-            -- - I need to ask refresh also to "auto-refresh-materialized-views" to ensure refresh "now" 
-            --   and not "when redshift want"; just in case an higher level materialized view depend on it
-            --   and need very fresh data.
-            -- AND autorefresh = 'f'  -- Intentionally commented out and kept for documentation purpose
-            and 
-              -- Split string to arrays and use them as second parameter of an in operator is too complex
-              -- I use some string matching
-              -- example ',views,sub_views' with no spaces
-	          quote_ident(',' + regexp_replace( schema_list, '\\s', '')  + ',') 
-              like
-              ('%,' + schema_name + ',%')
-        )
-      select
-        mv_schema,
-        mv_name,
-        mv_level
-      from
-        views_to_refresh
+      views_to_refresh as (
+        select
+          trim(schema_name) as mv_schema,
+          trim(name) as mv_name,
+          (state <> 1 ) as incremental_refresh_not_supported,
+          cast(REGEXP_REPLACE( name, 'mv_([0-9][0-9]).*', '$1') as integer) as mv_level
+        from
+          SVV_MV_INFO
+        where
+          is_stale = 't'
+          -- - I need to ask refresh also to "auto-refresh-materialized-views" to ensure refresh "now"
+          --   and not "when redshift want"; just in case an higher level materialized view depend on it
+          --   and need very fresh data.
+          -- AND autorefresh = 'f'  -- Intentionally commented out and kept for documentation purpose
+          and
+            -- Split string to arrays and use them as second parameter of an "in" operator is too complex
+            -- We use some string matching
+            -- example ',views,sub_views,' with no spaces
+          quote_ident(',' + regexp_replace( schema_list, '\\s', '')  + ',')
+            like
+            ('%,' + schema_name + ',%')
+      ),
+      refresh_history as (
+        SELECT
+          trim(schema_name) as mv_schema,
+          trim(mv_name) as mv_name,
+          start_time as refresh_start_time,
+          end_time as refresh_end_time,
+          ROW_NUMBER() over ( partition by database_name, schema_name, mv_name order by start_time desc ) as nr
+        FROM
+          SYS_MV_REFRESH_HISTORY
+        where
+          database_name = current_database()
+      ),
+      last_refresh as (
+        SELECT
+          *
+        FROM
+          refresh_history
+        where
+          nr = 1
+      )
+    select
+      v.mv_schema,
+      v.mv_name,
+      v.mv_level,
+      v.incremental_refresh_not_supported,
+      r.refresh_start_time as last_refresh_start_time,
+      r.refresh_end_time as last_refresh_end_time,
+      -- extract give null output for null input
+      extract(epoch from r.refresh_start_time) as last_refresh_start_time_epoch,
+      extract(epoch from r.refresh_end_time) as last_refresh_end_time_epoch
+    from
+      views_to_refresh v
+      left join last_refresh r on r.mv_schema  = v.mv_schema and r.mv_name  = v.mv_name
   );
   
   GRANT SELECT ON list_need_refresh_views_results to ${NAMESPACE}_mv_refresher_user;
