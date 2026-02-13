310c78950ef0b52d0a74b9f22803521ba5b4567c
Marco Vit <marco.vit@pagopa.it>
2026-02-13 17:37:53 +0100

    refactor(kpi,PIN-6997): reduce the number of subviews

--- views/application/R__application_mv_01_client_tenant_authserver_ips.sql
+++ views/application/R__application_mv_01_client_tenant_authserver_ips.sql
@@ -6,10 +6,17 @@ GRANT USAGE ON SCHEMA views TO ${NAMESPACE}_quicksight_user;
 DROP MATERIALIZED VIEW IF EXISTS views.mv_01_client_tenant_authserver_ips CASCADE;
 
 CREATE MATERIALIZED VIEW views.mv_01_client_tenant_authserver_ips AS 
-select  main.tenant_name, main.client_name, main.ips, latest_ts.latest_ts, oldest_ts.oldest_ts from sub_views.mv_00_client_tenant_authserver_ips__main main
-inner join sub_views.mv_00_client_tenant_authserver_ips__latest_ts latest_ts
-on main.client_id = latest_ts.client_id and main.ips = latest_ts.ips
-inner join sub_views.mv_00_client_tenant_authserver_ips__oldest_ts oldest_ts
-on main.client_id = oldest_ts.client_id and main.ips = oldest_ts.ips;
+select
+  t."name" tenant_name,
+  c."name" client_name,
+  main.client_id,
+  main.ips,
+  main.latest_ts,
+  main.oldest_ts
+from
+  sub_views.mv_00_client_tenant_authserver_ips__main main
+  inner join domains.client c on c.id = main.client_id
+  inner join domains.tenant t on t.id = c.consumer_id
+;
 
 GRANT SELECT ON TABLE views.mv_01_client_tenant_authserver_ips TO ${NAMESPACE}_quicksight_user;
