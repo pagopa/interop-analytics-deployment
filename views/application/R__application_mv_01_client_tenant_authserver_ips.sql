ad1df7bda5b5505c2b1661b7e174183d6d16dfbd
Marco Vit <marco.vit@pagopa.it>
2026-02-16 12:26:46 +0100

    style: Add old joins as comments. They are not equivalent due to delegation functionality. We have to decide which use for this use-case

--- views/application/R__application_mv_01_client_tenant_authserver_ips.sql
+++ views/application/R__application_mv_01_client_tenant_authserver_ips.sql
@@ -5,7 +5,7 @@ GRANT USAGE ON SCHEMA views TO ${NAMESPACE}_quicksight_user;
 
 DROP MATERIALIZED VIEW IF EXISTS views.mv_01_client_tenant_authserver_ips CASCADE;
 
-CREATE MATERIALIZED VIEW views.mv_01_client_tenant_authserver_ips AS 
+CREATE MATERIALIZED VIEW views.mv_01_client_tenant_authserver_ips AS
 select
   t."name" tenant_name,
   c."name" client_name,
@@ -17,6 +17,10 @@ from
   sub_views.mv_00_client_tenant_authserver_ips__main main
   inner join domains.client c on c.id = main.client_id
   inner join domains.tenant t on t.id = c.consumer_id
+  --inner join domains.client c on c.id = main.client_id
+  --inner join domains.client_purpose cp  on cp.client_id = c.id
+  --inner join domains.purpose p on p.id = cp.purpose_id
+  --inner join domains.tenant t on t.id = p.consumer_id
 ;
 
 GRANT SELECT ON TABLE views.mv_01_client_tenant_authserver_ips TO ${NAMESPACE}_quicksight_user;
