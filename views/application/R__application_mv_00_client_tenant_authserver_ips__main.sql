48826323440021e348f1ab5928a2e84b51ddf78d
marco-vit-pagopa <81150347+marco-vit-pagopa@users.noreply.github.com>
2026-02-19 17:53:40 +0100

    refactor(kpi,PIN-6997): performance optimization of IPs-by-client-view (#167)

--- views/application/R__application_mv_00_client_tenant_authserver_ips__main.sql
+++ views/application/R__application_mv_00_client_tenant_authserver_ips__main.sql
@@ -5,12 +5,17 @@ GRANT USAGE ON SCHEMA sub_views TO GROUP readonly_group;
 DROP MATERIALIZED VIEW IF EXISTS sub_views.mv_00_client_tenant_authserver_ips__main CASCADE;
 
 CREATE MATERIALIZED VIEW sub_views.mv_00_client_tenant_authserver_ips__main AUTO REFRESH NO as
-select t."name" tenant_name , gta.client_id client_id, c."name" client_name  , bra.requester_ip_address ips 
-from application.begin_request_audit bra
-inner join jwt.generated_token_audit gta on bra.correlation_id = gta.correlation_id
-inner join domains.client c on c.id = gta.client_id
-inner join domains.client_purpose cp  on cp.client_id = c.id
-inner join domains.purpose p on p.id = cp.purpose_id 
-inner join domains.tenant t on t.id = p.consumer_id
-where bra.service = 'authorization-server'
-group by t."name" , gta.client_id, c."name" , bra.requester_ip_address
+select
+  gta.client_id,
+  bra.requester_ip_address ips,
+  max(bra.timestamp) as latest_ts,
+  min(bra.timestamp) as oldest_ts
+from
+  application.begin_request_audit bra
+  inner join jwt.generated_token_audit gta
+          on bra.correlation_id = gta.correlation_id
+where
+  bra.service = 'authorization-server'
+group by
+  gta.client_id, bra.requester_ip_address
+;
